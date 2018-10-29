6c07a53cb8095d7adc6af16375c8776f46983a0b
Andreas Gropp <starmaerker@googlemail.com>
2018-10-29 05:47:15 +0100

    completed first version

--- index.js
+++ index.js
@@ -12,8 +12,12 @@ var paddleWidth = 100;
 var paddleX = (canvas.width - paddleWidth) / 2;
 var rightPressed = false;
 var leftPressed = false;
+var spacePressed = false;
+var score = 0;
+var speed = 10;
+var lives = 3;
 
-var brickRowCount = 6;
+var brickRowCount = 1;
 var brickColumnCount = 11;
 var brickWidth = 75;
 var brickHeight = 20;
@@ -21,19 +25,19 @@ var brickPadding = 10;
 var brickOffsetTop = 30;
 var brickOffsetLeft = 30;
 var bricks = [];
-for(var c=0; c<brickColumnCount; c++) {
+for (var c = 0; c < brickColumnCount; c++) {
     bricks[c] = [];
-    for(var r=0; r<brickRowCount; r++) {
+    for (var r = 0; r < brickRowCount; r++) {
         bricks[c][r] = { x: 0, y: 0, status: 1 };
     }
 }
 
 function drawBricks() {
-    for(var c=0; c<brickColumnCount; c++) {
-        for(var r=0; r<brickRowCount; r++) {
-            if(bricks[c][r].status == 1) {
-                var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
-                var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+    for (var c = 0; c < brickColumnCount; c++) {
+        for (var r = 0; r < brickRowCount; r++) {
+            if (bricks[c][r].status == 1) {
+                var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+                var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
                 bricks[c][r].x = brickX;
                 bricks[c][r].y = brickY;
                 ctx.beginPath();
@@ -47,17 +51,35 @@ function drawBricks() {
 }
 
 function collisionDetection() {
-    for(var c=0; c<brickColumnCount; c++) {
-        for(var r=0; r<brickRowCount; r++) {
+    for (var c = 0; c < brickColumnCount; c++) {
+        for (var r = 0; r < brickRowCount; r++) {
             var b = bricks[c][r];
-            if(b.status == 1) {
-                if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+            if (b.status == 1) {
+                if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
                     dy = -dy;
                     b.status = 0;
+                    score++;
+                    if (score == brickRowCount * brickColumnCount) {
+                        alert("Next Round");
+                        document.location.reload();
+                        setInterval(draw, speed--);
+                    }
                 }
             }
         }
     }
+}
+
+function drawScore() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = randColour;
+    ctx.fillText("Score: " + score, 8, 20);
+}
+
+function drawLives() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = randColour;
+    ctx.fillText("Lives: " + lives, canvas.width - 65, 20);
 }
 
 function getRandomInt(min, max) {
@@ -85,9 +107,13 @@ function draw() {
     drawBricks();
     drawBall();
     drawPaddle();
+    drawScore();
+    drawLives();
     collisionDetection();
     x += dx;
     y += dy;
+
+
 
     if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
@@ -104,10 +130,22 @@ function draw() {
             dy = -dy;
             //randColour = colourArray[getRandomInt(0, colourArray.length - 1)];
             randColour = '#' + (0x1000000 + (Math.random()) * 0xffffff).toString(16).substr(1, 6);
+
         }
         else {
-            alert("GAME OVER");
-            document.location.reload();
+            lives--;
+            if (!lives) {
+                alert("GAME OVER");
+                document.location.reload();
+            }
+            else {
+                x = canvas.width / 2;
+                y = canvas.height - 30;
+                dx = 2;
+                dy = -2;
+                paddleX = (canvas.width - paddleWidth) / 2;
+            }
+
         }
 
     }
@@ -118,6 +156,8 @@ function draw() {
     else if (leftPressed && paddleX > 0) {
         paddleX -= 4;
     }
+
+
 }
 
 document.addEventListener("keydown", keyDownHandler, false);
@@ -130,6 +170,9 @@ function keyDownHandler(e) {
     else if (e.keyCode == 37) {
         leftPressed = true;
     }
+    else if (e.keyCode == 32) {
+        spacePressed = true;
+    }
 }
 
 function keyUpHandler(e) {
@@ -139,7 +182,12 @@ function keyUpHandler(e) {
     else if (e.keyCode == 37) {
         leftPressed = false;
     }
+    else if (e.keyCode == 32) {
+        spacePressed = false;
+    }
 }
 
-setInterval(draw, 10);
+setInterval(draw, speed);
+
+
 
